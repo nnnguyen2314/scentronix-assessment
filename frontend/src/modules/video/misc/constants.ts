cdb6752c6c4bc0c8ffe7a646ad380422764927a9
gerald-nnnguyen-da <nguyennhat.nguyen@digitalavenues.com>
2024-07-18 01:16:57 +0700

    Frontend task

--- frontend/src/modules/video/misc/constants.ts
+++
@@ -1,6 +0,0 @@
-export const VIDEO_LIST_API_URL = '/videos';
-export const VIDEO_DETAIL_API_URL = '/video/:videoId';
-export const FETCH_VIDEO_INFO_API_URL = 'https://www.googleapis.com/youtube/v3/videos';
-export const VIDEO_CREATE_API_URL = '/video';
-
-export const YOUTUBE_VIDEO_KEY = 'AIzaSyA5YEw2RGvy1ayky2XDPqz-ky2ahd7ttA4';
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,10 @@
 import type { NextPage } from "next";
 import RootLayout from "@modules/shared/features/layout/RootLayout";
-import VideoListContainerWithPlaying from "@modules/video/features/videoList/containers/VideoListContainerWithPlaying";
 
 const IndexPage: NextPage = () => {
     return (
         <RootLayout>
-            <VideoListContainerWithPlaying />
+            <div>Home</div>
         </RootLayout>
     );
 };
